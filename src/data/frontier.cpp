23c8270d2e08400ff2a1777bf354c2b88fbcd0fe
MrPekar98 <martin.pekar@outlook.dk>
2020-09-08 09:49:32 +0200

    URL frontier.

---
+++ src/data/frontier.cpp
@@ -0,0 +1,46 @@
+#include "frontier.hpp"
+
+namespace crawler
+{
+    // Checks whether URL already exists in queue.
+    bool frontier::url_exists(const std::string& url) noexcept
+    {
+        bool ret = false;
+
+        for (int i = 0; i < this->urls.size(); i++)
+        {
+            std::string temp = this->urls.front();
+
+            if (temp.compare(url) == 0)
+                ret = true;
+
+            this->urls.pop();
+            this->urls.push(temp);
+        }
+
+        return ret;
+    }
+
+    // Adds URL if not already added.
+    void frontier::add_url(const std::string& url) noexcept
+    {
+        if (this->urls.empty() || !url_exists(url))
+            this->urls.push(url);
+    }
+
+    // TODO: Use optional here.
+    // Returns next URL in line and removes it.
+    const std::string& frontier::get_next() noexcept
+    {
+        std::string& next = this->urls.front();
+        this->urls.pop();
+
+        return next;
+    }
+
+    // Tells wether frontier is empty.
+    bool frontier::empty() const noexcept
+    {
+        return this->urls.empty();
+    }
+}
---
+++ src/data/frontier.hpp
@@ -0,0 +1,18 @@
+#include <queue>
+#include <string>
+
+namespace crawler
+{
+    // Simple frontier with no prioritization and politenes..
+    class frontier
+    {
+    private:
+        std::queue<std::string> urls;
+        bool url_exists(const std::string& url) noexcept;
+
+    public:
+        void add_url(const std::string& url) noexcept;
+        const std::string& get_next() noexcept;
+        bool empty() const noexcept;
+    };
+}
