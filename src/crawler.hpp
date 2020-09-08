81795b292337e823fa15016f5380ca3ea5d71515
MrPekar98 <martin.pekar@outlook.dk>
2020-09-08 17:07:34 +0200

    Reverted.

--- src/crawler.hpp
+++ src/crawler.hpp
@@ -4,6 +4,6 @@
 namespace crawler
 {
     // Prototypes.
-    std::vector<std::vector<std::string>> crawl(const std::vector<std::string>& urls);
-    std::vector<std::vector<std::string>> crawl(const std::vector<std::string>& urls, unsigned long doc_limit);
+    std::vector<std::string> crawl(const std::vector<std::string>& urls);
+    std::vector<std::string> crawl(const std::vector<std::string>& urls, unsigned long doc_limit);
 }
