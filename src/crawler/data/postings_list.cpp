981f45e748045eb3a131df525608f3d54fec925d
MrPekar98 <martin.pekar@outlook.dk>
2020-09-15 14:33:26 +0200

    Postings setup.

--- src/crawler/data/postings_list.cpp
+++ src/crawler/data/postings_list.cpp
@@ -1,6 +1,41 @@
 #include "postings_list.hpp"
 
-namespace indexing
+namespace scam::crawler::indexing
 {
-    
+    // Constructor.
+    postings_list::postings_list(const std::vector<document>& docs) noexcept
+        : docs(docs)
+    {
+
+    }
+
+    // Builds inverted index.
+    void postings_list::build_postings() noexcept
+    {
+
+    }
+
+    // Updates inverted index.
+    void postings_list::reload(const std::vector<document>& docs) noexcept
+    {
+
+    }
+
+    // Searches inverted index by given query. Returns ranked list of URLS.
+    std::vector<std::string> postings_list::search(const std::string& query) const noexcept
+    {
+
+    }
+
+    // Checks for existence of word in inverted index.
+    bool postings_list::word_exists(const std::string& word) const noexcept
+    {
+        for (std::map<std::string, std::vector<unsigned>>::iterator it = this->postings.begin(); it != this->postings.end(); it++)
+        {
+            if (word.compare(it->first) == 0)
+                return true;
+        }
+
+        return false;
+    }
 }
--- src/crawler/data/postings_list.hpp
+++ src/crawler/data/postings_list.hpp
@@ -1,12 +1,21 @@
-#include <list>
+#include <vector>
+#include <map>
+#include <string>
+#include "document.hpp"
 
-namespace indexing
+namespace scam::crawler::indexing
 {
     class postings_list
     {
     private:
+        std::map<std::string, std::vector<unsigned>> postings;
+        std::vector<document> docs;
+        void build_postings() noexcept;
+        bool word_exists(const std::string& word) const noexcept;
 
     public:
-
+        postings_list(const std::vector<document>& docs) noexcept;
+        void reload(const std::vector<document>& docs) noexcept;
+        std::vector<std::string> search(const std::string& query) const noexcept;
     };
 }
