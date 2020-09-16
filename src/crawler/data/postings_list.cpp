d7bb5728221063445e9d9d52a9e4112a9997c143
MrPekar98 <martin.pekar@outlook.dk>
2020-09-16 14:26:53 +0200

    Base version.

--- src/crawler/data/postings_list.cpp
+++ src/crawler/data/postings_list.cpp
@@ -7,26 +7,51 @@ namespace scam::crawler::indexing
     postings_list::postings_list(const std::vector<document>& docs) noexcept
         : docs(docs)
     {
-
+        build_postings();
     }
 
     // Builds inverted index.
     void postings_list::build_postings() noexcept
     {
+        this->building = true;
+        unsigned length = this->docs.size();
 
+        for (unsigned i = 0; i < length; i++)
+        {
+            std::set<std::string> ts = postings_list::terms(this->docs[i].content);
+
+            for (std::set<std::string>::iterator it = ts.begin(); it != ts.end(); it++)
+            {
+                if (!word_exists(*it))
+                    this->postings[*it] = std::vector<unsigned>();
+                
+                this->postings[*it].push_back(this->docs[i].id);
+            }
+        }
+
+        this->building = false;
     }
 
     // Updates inverted index.
     void postings_list::reload(const std::vector<document>& docs) noexcept
     {
+        this->docs.clear();
+        unsigned length = docs.size();
 
+        for (unsigned i = 0; i < length; i++)
+        {
+            this->docs.push_back(docs[i]);
+        }
+
+        build_postings();
     }
 
     // Searches inverted index by given query. Returns ranked list of URLS.
-    std::vector<std::string> postings_list::search(const std::string& query) const noexcept
+    // Must wait till building is done.
+    /*std::vector<std::string> postings_list::search(const std::string& query) const noexcept
     {
-
-    }
+        while (this->building);
+    }*/
 
     // Checks for existence of word in inverted index.
     bool postings_list::word_exists(const std::string& word) const noexcept
@@ -41,5 +66,35 @@ namespace scam::crawler::indexing
         {
             return false;
         }
+    }
+
+    // TODO: Add functions to manipulate each term.
+    // Returns set of terms.
+    // Stop words are removed, normalization, and stemming.
+    std::set<std::string> postings_list::terms(const std::string& str) noexcept
+    {
+        std::set<std::string> terms;
+        std::string temp = "";
+        unsigned length = str.length();
+
+        for (unsigned i = 0; i < length; i++)
+        {
+            if (str[i] == ' ')
+            {
+                terms.insert(temp);
+                temp = "";
+            }
+
+            else if (i == length - 1)
+            {
+                temp += str[i];
+                terms.insert(temp);
+            }
+
+            else
+                temp += str[i];
+        }
+
+        return terms;
     }
 }
--- src/crawler/data/postings_list.hpp
+++ src/crawler/data/postings_list.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <set>
 #include "document.hpp"
 
 namespace scam::crawler::indexing
@@ -10,8 +11,10 @@ namespace scam::crawler::indexing
     private:
         std::map<std::string, std::vector<unsigned>> postings;
         std::vector<document> docs;
+        bool building = false;
         void build_postings() noexcept;
         bool word_exists(const std::string& word) const noexcept;
+        static std::set<std::string> terms(const std::string& str) noexcept;
 
     public:
         postings_list(const std::vector<document>& docs) noexcept;
