8eabef3d8da959d0263bce453a548b9e849b213c
MrPekar98 <martin.pekar@outlook.dk>
2020-09-18 09:54:17 +0200

    Class setup.

--- src/indexing/term.hpp
+++ src/indexing/term.hpp
@@ -1,9 +1,24 @@
 #ifndef TERM_HPP
 #define TERM_HPP
 
+#include <string>
+
 namespace scam::indexing
 {
     // TODO: Add function to tokenize, stem, and so on...
+    class term
+    {
+    private:
+        std::string str;
+
+    public:
+        term(const std::string& str) noexcept;
+        std::string get_str() const noexcept;
+        term& tokenize() noexcept;
+        term& normalize() noexcept;
+        term& stem() noexcept;
+        bool is_stop_word() const noexcept;
+    };
 }
 
 #endif
