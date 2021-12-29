3229ab22fd15c50834f670057dc9977bdf39279e
MrPekar98 <martin.pekar@outlook.dk>
2021-12-29 23:24:10 +0100

    Added default elements

--- include/util/pair.hpp
+++ include/util/pair.hpp
@@ -12,6 +12,11 @@ struct Pair
     constexpr Pair(T1&& first, T2&& second)
         : first(std::forward<T1>(first)), second(std::forward<T2>(second))
     {}
+
+    constexpr Pair(const Pair<T1, T2>& other) = default;
+    constexpr Pair(Pair<T1, T2>&& other) = default;
+    constexpr Pair& operator=(const Pair& other) = default;
+    constexpr Pair& operator=(Pair&& other) = default;
 };
 
 #endif
